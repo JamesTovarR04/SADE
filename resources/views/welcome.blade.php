911d183512472a0066c8e5fc5e49cc8b55b40fdb
JamesTovarR04 <jamestovar09@hotmail.com>
2021-01-28 17:58:02 -0500

    Documentacion y mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SAPC
                 </div>
 
                 <div class="links">
