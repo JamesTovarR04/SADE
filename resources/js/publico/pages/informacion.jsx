5fc380dd26da6684b9e5c1ad1351c8a8ecd8e7a3
jodaor <jo.selo01@hotmail.com>
2021-02-17 23:43:07 -0500

    se creo la vista de la pagina información

--- resources/js/publico/pages/informacion.jsx
+++ resources/js/publico/pages/informacion.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
+
 const Informacion = () => {
 
-    return <h1>Informacion</h1>
+    return <div className="card mb-3">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src="/images/col.jpg" alt="..."/>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                <h5 className="card-title">Nombre Completo De La Escuela</h5>
+                <p className="card-text">Pocas escuelas son tan exitosas como la Academia Kunugigaoka. Es más conocida como una secundaria, pero también tiene una preparatoria para los alumnos con mejores calificaciones. Lo que hace a esta academia diferente es que es la única escuela que maneja un sistema de avance a costa de otros alumnos.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
 }
 
